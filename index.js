d129211a0de42cd28f4d7b3ceecb16c823886bc2
Harry Allen <66224939+MajesticString@users.noreply.github.com>
2022-03-23 22:31:43 -0700

    Initial commit

---
+++ index.js
@@ -0,0 +1,25 @@
+import { Intents } from 'discord.js';
+import { Client } from 'discord.js';
+
+const CHANNEL_ID = '500102969702875156';
+const TOKEN = 'replace with token';
+
+process.on('uncaughtException', console.error);
+
+const client = new Client({
+  intents: [
+    Intents.FLAGS.GUILDS,
+    Intents.FLAGS.GUILD_MESSAGES,
+    Intents.FLAGS.GUILD_MESSAGE_REACTIONS,
+  ],
+});
+
+client.on('ready', () => console.log('ready'));
+client.on('messageCreate', async (message) => {
+  if (message.channelId === CHANNEL_ID) {
+    await message.react('👍');
+    await message.react('👎');
+  }
+});
+
+client.login(TOKEN);
